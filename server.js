17f35a17a07dfa856d08837bcf57d7722ec3cc50
Unknown <petercarmines@gmail.com>
2018-05-16 21:23:20 -0400

    sequelize in scrape file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var db = require("./models");
 require("./routes/api-routes.js")(app);
 
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
