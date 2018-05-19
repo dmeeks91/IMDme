061c67c35bdf81645014e84896c723983eddc16c
Dominique Meeks <dominique.l.meeks@gmail.com>
2018-05-19 14:57:43 -0400

    Heroku and Jaws
    link it up

--- models/index.js
+++ models/index.js
@@ -4,13 +4,15 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
+var env       = "mysql://zpthmptycjmfvpx0:zios9su111ch0wue@ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vt0wlcqyffbu6xra";//process.env.JAWSDB_URL;
 var config    = require(__dirname + '/../config/config.json')[env];
-var env       = process.env.JAWSDB_URL || config['development'];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(env, config); //process.env[config.use_env_variable]
-} else {
+var sequelize = new Sequelize(env);
+if (!config) {
+  var sequelize = new Sequelize(env); //process.env[config.use_env_variable]
+}  
+else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
