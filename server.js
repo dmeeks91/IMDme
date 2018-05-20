a499598085a21b9d9e9f223c714e2aac4d258542
Unknown <erinlbritton@gmail.com>
2018-05-20 16:48:32 -0400

    SQL queries to GET data for profile page and d3.js nodes/links

--- server.js
+++ server.js
@@ -28,7 +28,16 @@ app.use(routes);
   var imdb = new IMDBconstruct();
   //imdb.init("","nm0004898");
   //imdb.init("","nm7458565");
-  //imdb.init("","nm0005125");
+  imdb.init("","nm3357095");
+// nm0004898
+// nm0005125
+// nm0628601
+// nm0940990
+// nm1440281
+// nm1642445
+// nm2656455
+// nm3053439
+// nm3357095
  
 /*IMDB.getCast("https://www.imdb.com/title/tt3590068/fullcredits?ref_=tt_cl_sm#cast"); */
 
