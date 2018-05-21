922dbdb23aa0c84fd5d59d2d4941fd8664b8509a
Unknown <erinlbritton@gmail.com>
2018-05-20 20:21:03 -0400

    d3.js

--- www/assets/javascript/routes.js
+++ www/assets/javascript/routes.js
@@ -33,8 +33,8 @@ routes = [
     },
   },
   {
-    path: '/feed',
-    componentUrl: './assets/pages/feed.html',
+    path: '/network',
+    componentUrl: './assets/pages/network.html',
   },
   {
     path: '/profile',
